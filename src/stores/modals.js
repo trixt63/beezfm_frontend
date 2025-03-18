29e0d7f8c96e22f3413328ea78353003e1715796
trixt63 <xuantung.trinh.63@gmail.com>
2025-03-19 04:01:31 +0700

    add template in src

---
+++ src/stores/modals.js
@@ -0,0 +1,15 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useModalsStore = defineStore('modals', () => {
+  const modals = ref([])
+  function addModal(modal) {
+    modals.value.push(modal)
+  }
+
+  function removeModal(id) {
+    modals.value = modals.value.filter(modal => modal.id !== id)
+  }
+
+  return { modals, addModal, removeModal }
+})
