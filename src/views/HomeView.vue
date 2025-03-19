c2f6aa9bf7989bebd57c276ced24ea7f42b201bd
tungtx <tungtx@gem-corp.tech>
2025-03-19 12:30:28 +0700

    change endpoints: object -> objects

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,7 +24,7 @@ onMounted(() => {
 });
 
 const getHotels = async () => {
-  await axios.get(`${VITE_API_URL}/object/tree?limit=100&offset=0`)
+  await axios.get(`${VITE_API_URL}/objects/tree?limit=100&offset=0`)
     .then((response) => {
       hotels.value = response.data;
       handleHotelClick(selectedHotel.value || hotels.value[0]);
@@ -35,7 +35,7 @@ const getHotels = async () => {
 
 const getObjectDetails = async () => {
   if (!selectedRoom.value) return;
-  await axios.get(`${VITE_API_URL}/object/${selectedRoom.value.id}?include_children=true&include_datapoints=true`)
+  await axios.get(`${VITE_API_URL}/objects/${selectedRoom.value.id}?include_children=true&include_datapoints=true`)
     .then((response) => {
       objects.value = response.data;
     }).catch((error) => {
@@ -73,7 +73,7 @@ const closeModalCreate = async () => {
 };
 
 const onCreateObject = async (data) => {
-  await axios.post(`${VITE_API_URL}/object`, data)
+  await axios.post(`${VITE_API_URL}/objects`, data)
     .then(() => {
       getHotels();
     }).catch((error) => {
@@ -83,7 +83,7 @@ const onCreateObject = async (data) => {
 };
 
 const handleDeleteObject = async (object) => {
-  await axios.delete(`${VITE_API_URL}/object/${object.id}`).then(async () => {
+  await axios.delete(`${VITE_API_URL}/objects/${object.id}`).then(async () => {
     await getHotels();
   }).catch((error) => {
     console.error(error);
@@ -91,7 +91,7 @@ const handleDeleteObject = async (object) => {
 }
 
 const handleFindObject = async (object) => {
-  await axios.get(`${VITE_API_URL}/object/query/${object.id}/${object.path}`)
+  await axios.get(`${VITE_API_URL}/objects/query/${object.id}/${object.path}`)
     .then((response) => {
       console.log(response.data);
     }).catch((error) => {
@@ -100,7 +100,7 @@ const handleFindObject = async (object) => {
 }
 
 const handleSaveObject = async (object) => {
-  await axios.put(`${VITE_API_URL}/object/${object.id}`, object)
+  await axios.put(`${VITE_API_URL}/objects/${object.id}`, object)
     .then(() => {
       getHotels();
     }).catch((error) => {
